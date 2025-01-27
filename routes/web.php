aef89baea97bfb086791672d54d2a698d597bc74
sadique-cws <sadique.hussain96@gmail.com>
2025-01-27 17:12:30 +0530

    add controllers and web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ExamCenterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RecruiterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("admin")->group(function(){
+   Route::controller(AdminController::class)->group(function(){
+
+   });
+});
+
+Route::prefix("recruiter")->group(function(){
+    Route::controller(RecruiterController::class)->group(function(){
+
+    });
+});
+
+Route::prefix("exam-center")->group(function(){
+    Route::controller(ExamCenterController::class)->group(function(){
+
+    });
+});
+
+// public routes
+
+Route::controller(HomeController::class)->group(function(){
+    Route::get("/", "index")->name("public.index");
 });
